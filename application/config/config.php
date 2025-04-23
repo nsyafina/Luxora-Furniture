55cb9bebb6b081006631d365bbf620acc38e2d45
nsyafina <najlasfina13@gmail.com>
2025-04-23 11:40:26 +0700

    update

--- application/config/config.php
+++ application/config/config.php
@@ -23,7 +23,7 @@ defined('BASEPATH') or exit('No direct script access allowed');
 | a PHP script and you can easily do that on your own.
 |
 */
-$config['base_url'] = 'http://192.168.43.64/luxora-interiors/';
+$config['base_url'] = 'http://localhost/luxora-interiors/';
 
 /*
 |--------------------------------------------------------------------------
